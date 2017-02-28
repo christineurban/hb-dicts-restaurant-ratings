22877b99835d2822338fbb614048e9ce80f56955
Hackbright Student <no-reply@hackbrightacademy.com>
2017-02-27 17:44:47 -0800

    Completed function which includes user input, added docstrings

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -1,5 +1,6 @@
-
 def alpha_rest_ratings(rest_scores):
+    """ Alphabetizes restaurant ratings """
+
     restaurant_ratings = open(rest_scores)
 
     rate_restaurants = {}
@@ -7,16 +8,28 @@ def alpha_rest_ratings(rest_scores):
     for line in restaurant_ratings:
         rest_name, rating = line.strip().split(':')
 
-        rate_restaurants[rest_name] = rating
+        rate_restaurants[rest_name] = int(rating)
 
+    restaurant_ratings.close()    
+
+    user_rest_name = raw_input("Enter your restaurant name: ")
+    
+    print "How would you rate this restaurant from 1 to 5?"
+    user_rest_rating = int(raw_input("1 = terrible, 5 = excellent: "))
+
+    rate_restaurants[user_rest_name] = user_rest_rating
+        
     rest_by_alpha = sorted(rate_restaurants.items())
     
-
-
     for restaurant, rating in rest_by_alpha:
-        print "%s is rated at %s" % (restaurant, rating)    
+        print "%s is rated at %d" % (restaurant, rating)    
 
-alpha_rest_ratings('scores.txt')    
+
+alpha_rest_ratings('scores.txt')
+
+       
+
+
 
 
 
